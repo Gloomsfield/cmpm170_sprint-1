4e428ee19f26fb08752910becfac9219c1cf3544
Gloomsfield <amory@acosta.boo>
2026-04-10 11:27:56 -0700

    spawn game scene on desktop

--- src/scenes/desktop.js
+++ src/scenes/desktop.js
@@ -7,6 +7,7 @@ class Desktop extends Phaser.Scene {
 		this.currency_interface = new CurrencyInterface(this);
 
 		this.scene.launch("email-window_scene");
+		this.scene.launch("game-window_scene");
 
 		this.scene.get("email-window_scene").events.on("sort-correct", () => {
 			this.currency_interface.money_earn(5.0); // TODO factor amount into config
